223565d7dc961627fcd2dbb9d0065d7adaf8233d
Haochen Ding <haochendingarven@gmail.com>
2020-11-26 09:09:19 -0500

    Fixed some failing queries

--- tpc-ds-2.4/openLookeng/tpcds_2_4_presto/q30.sql
+++ tpc-ds-2.4/openLookeng/tpcds_2_4_presto/q30.sql
@@ -1,27 +1,48 @@
 ---q30-v2.4---
 
- with customer_total_return as
- (select wr_returning_customer_sk as ctr_customer_sk
-        ,ca_state as ctr_state,
- 	sum(wr_return_amt) as ctr_total_return
- from web_returns, date_dim, customer_address
- where wr_returned_date_sk = d_date_sk
-   and d_year = 2002
-   and wr_returning_addr_sk = ca_address_sk
- group by wr_returning_customer_sk,ca_state)
- select c_customer_id,c_salutation,c_first_name,c_last_name,c_preferred_cust_flag
-       ,c_birth_day,c_birth_month,c_birth_year,c_birth_country,c_login,c_email_address
-       ,c_last_review_date,ctr_total_return
- from customer_total_return ctr1, customer_address, customer
- where ctr1.ctr_total_return > (select avg(ctr_total_return)*1.2
- 			  from customer_total_return ctr2
-                  	  where ctr1.ctr_state = ctr2.ctr_state)
-       and ca_address_sk = c_current_addr_sk
-       and ca_state = 'GA'
-       and ctr1.ctr_customer_sk = c_customer_sk
- order by c_customer_id,c_salutation,c_first_name,c_last_name,c_preferred_cust_flag
-                  ,c_birth_day,c_birth_month,c_birth_year,c_birth_country,c_login,c_email_address
-                  ,c_last_review_date,ctr_total_return
- limit 100
+ WITH
+  customer_total_return AS (
+   SELECT
+     "wr_returning_customer_sk" "ctr_customer_sk"
+   , "ca_state" "ctr_state"
+   , "sum"("wr_return_amt") "ctr_total_return"
+   FROM
+     web_returns
+   , date_dim
+   , customer_address
+   WHERE ("wr_returned_date_sk" = "d_date_sk")
+      AND ("d_year" = 2002)
+      AND ("wr_returning_addr_sk" = "ca_address_sk")
+   GROUP BY "wr_returning_customer_sk", "ca_state"
+) 
+SELECT
+  "c_customer_id"
+, "c_salutation"
+, "c_first_name"
+, "c_last_name"
+, "c_preferred_cust_flag"
+, "c_birth_day"
+, "c_birth_month"
+, "c_birth_year"
+, "c_birth_country"
+, "c_login"
+, "c_email_address"
+, "c_last_review_date_sk"
+, "ctr_total_return"
+FROM
+  customer_total_return ctr1
+, customer_address
+, customer
+WHERE ("ctr1"."ctr_total_return" > (
+      SELECT ("avg"("ctr_total_return") * DECIMAL '1.2')
+      FROM
+        customer_total_return ctr2
+      WHERE ("ctr1"."ctr_state" = "ctr2"."ctr_state")
+   ))
+   AND ("ca_address_sk" = "c_current_addr_sk")
+   AND ("ca_state" = 'GA')
+   AND ("ctr1"."ctr_customer_sk" = "c_customer_sk")
+ORDER BY "c_customer_id" ASC, "c_salutation" ASC, "c_first_name" ASC, "c_last_name" ASC, "c_preferred_cust_flag" ASC, "c_birth_day" ASC, "c_birth_month" ASC, "c_birth_year" ASC, "c_birth_country" ASC, "c_login" ASC, "c_email_address" ASC, "c_last_review_date_sk" ASC, "ctr_total_return" ASC
+LIMIT 100
 
 ;
